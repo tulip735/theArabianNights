3f03ee02cfcfd4e7681c11cdf964b41b04c453b3
blackTulip <ccd1035948164@gmail.com>
2020-01-30 18:00:33 +0800

    java letterCombinations

---
+++ restoreIpAddresses/restoreIpAddresses.java
@@ -0,0 +1,51 @@
+
+import java.util.*;
+
+
+public class restoreIpAddresses {
+    /**
+      Given a string containing only digits, 
+      restore it by returning all possible valid IP address combinations.
+      Example:
+      Input: "25525511135"
+      Output: ["255.255.11.135", "255.255.111.35"]
+    */
+    public static List<String> restoreIpAddresses(String s) {
+        List<String> result = new ArrayList<>();
+        
+
+        return result;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int x = '5' - '0';
+        System.out.println(x);
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
