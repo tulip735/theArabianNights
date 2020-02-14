65e2f6e516edcbd5b20758c5c103ed0a9e078e33
blackTulip <ccd1035948164@gmail.com>
2020-02-14 23:44:28 +0800

    array singleNumber

--- singleNumberThree/SingleNumber.java
+++ singleNumberThree/SingleNumber.java
@@ -1,13 +1,7 @@
-
-
 import java.util.*;
 
 class SingleNumber {
     
-
-
-
-
     /**
       Given a non-empty array of integers, every element appears three times except for one, 
       which appears exactly once. Find that single one.
@@ -34,9 +28,32 @@ class SingleNumber {
         return one;
     }
 
+    public static int singleNumberII(int[] nums) {
+        // 利用数组存储每一位
+        int result = 0;
+        final int w = Integer.SIZE;
+        System.out.println(w);
+        int[] count = new int[w];
+        for (int i:nums) {
+            for (int j = 0; j < w; j++) {
+                count[j] += (i >> j) & 1;
+                count[j] = count[j] % 3;
+            }
+        }
+
+        for (int j = 0; j < w; j++) {
+            System.out.print(count[j]);
+            System.out.print('\t');
+            result += (count[j] << j);
+        }
+        System.out.print('\n');
+        System.out.println(result);
+        return result;
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[] {2,2,3,2};
-        int x = singleNumber(nums);
+        int x = singleNumberII(nums);
         
     }
 }
