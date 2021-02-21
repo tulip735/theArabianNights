9ab038766c0b321ab26461c61dd2606bdc965b4f
blackTulip <ccd1035948164@gmail.com>
2021-02-22 00:26:44 +0800

    Tree populating next right pointer whit null node

--- tree/populatingNextRightPointers/PopulatingNextRightPointers.java
+++ tree/populatingNextRightPointers/PopulatingNextRightPointers.java
@@ -61,6 +61,47 @@ public class PopulatingNextRrightPointers {
         connectDFS(root.left);
         connectDFS(root.right);
     }
+    /**
+      Populate each next pointer to point to its next right node. 
+      If there is no next right node, the next pointer should be set to NULL.
+      Initially, all next pointers are set to NULL.
+      
+            1                  1 -> null
+      
+         2     3           2 ->   3 -> null
+      
+      4         7     4      ->       7 -> null
+
+    */
+
+
+    Node connectII(Node root) {
+        Node cur = root;
+        while (cur != null) {
+            Node next = null;
+            Node pre = null;
+            while (cur != null) {
+                if (next == null)
+                    next = cur.left != null ? cur.left : cur.right;
+
+                if (cur.left != null) {
+                    if (pre != null)
+                        pre.next = cur.left;
+                    // 加入一个pre指针，指向前一个节点,这一条语句一定执行
+                    pre = cur.left;
+                }
+
+                if (cur.right != null) {
+                    if (pre != null)
+                        pre.next = cur.right;
+                    pre = cur.right;
+                }
+                cur = cur.next;
+            }
+            cur = next;
+        }
+    }
+
 
 
     public static void main(String[] args) {
